5681228ad13ad0492845ab8defe87e47269960ce
Chen Youxue <596798600@qq.com>
2014-05-12 18:32:38 -0400

    first comment

---
+++ sample class.h
@@ -0,0 +1,13 @@
+//
+//  sample class.h
+//  my first gitup
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 Chen Youxue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sample_class : NSObject
+
+@end
---
+++ sample class.m
@@ -0,0 +1,13 @@
+//
+//  sample class.m
+//  my first gitup
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 Chen Youxue. All rights reserved.
+//
+
+#import "sample class.h"
+
+@implementation sample_class
+
+@end
